2388463ec0e87d9789ad16072edb465c3f86dc3b
347255699 <13710414456@163.com>
2017-03-29 15:58:43 +0800

    添加全局访问内  warning modal 显示插件

--- src/main/resources/static/custom/common/global.js
+++ src/main/resources/static/custom/common/global.js
@@ -35,9 +35,43 @@ var Global = (function () {
         } // 通知类型对应的调整
         new PNotify(notification); // 创建通知
     };
+
+    /**
+     * 警告
+     * @param title
+     * @param content
+     * @param callback
+     * @param type 警告类型{warning|danger}
+     */
+    var warning = function (title, content, type, callback) {
+        var ids = {
+            "btn": "#btn_warning_modal_confirm",
+            "h4": "#warning_modal_title_h4",
+            "title": "#warning_modal_title",
+            "content": "#warning_modal_content",
+            "modal": "#global_warning_modal"
+        }; // 元素id
+
+        if (type == "danger") {
+            $(ids.h4).addClass("red");
+            $(ids.btn).addClass("btn-danger");
+        } else {
+            $(ids.h4).addClass("orange");
+            $(ids.btn).addClass("btn-warning");
+        } // 调整样式
+
+        $(ids.title).text(title); // 设置标题
+        $(ids.content).text(content); // 设置内容
+        $(ids.btn).click(function () {
+            callback();
+        }); // 绑定确定按钮
+        $(ids.modal).modal(); // 显示模态框
+    };
+
     return {
         init: init,
-        notify: notify
+        notify: notify,
+        warning: warning
     };
 })();
 
