95adb04e02d31ceeb7cdea2c70e0ba5d51d6722e
347255699 <13710414456@163.com>
2017-03-31 01:12:55 +0800

    为dataTable强化插件添加默认功能，包括删除功能、添加and修改Modal接口。添加完整实例...  还有锋哥棒棒的！

--- src/main/resources/static/custom/common/global.js
+++ src/main/resources/static/custom/common/global.js
@@ -16,11 +16,11 @@ var Global = (function () {
      */
     var notify = function (title, content, type) {
         var notification = {
-            title: title,
-            text: content,
+            title: title || "",
+            text: content || "",
             type: 'info',
             styling: 'bootstrap3',
-            delay: 3000  // 停留时间
+            delay: 2000  // 停留时间
         }; // 通知实体
         switch (type) {
             case 'warning':
@@ -89,14 +89,22 @@ var Global = (function () {
 
 /**
  * dataTable强化类
- * @description 对原生table进行功能强化
+ * @description 对原生table进行功能强化,默认提供删除功能。添加和更改功能需额外通过绑定用户提供的modalId和callback方法来启用
+ * 注意事项：默认启用springmvc @RestController，后台需使用对应的RequestMethod.delete来映射删除功能即数据请求和删除请求的地址为同一个，用delete和post来区分
  * 使用方式:
  *          var table = new DataTablePlus({'targetId':'#xxx','columns':[{'name':'xxx','title':'xxxx'},{...},...],'dataUrl':'xxxUrl'});
  *          var dataTable = table.build(); // 返回原生dataTable API
  * option:
- *          targetId : 目标dom元素id,需带上'#'符号
- *          columns  : 列表项基础信息，注意列表默认首列启用checkbox，checkbox自动定义id作为val,所以无需重复为列表定义id属性或checkbox元素
- *          dataUrl  : 数据请求地址
+ *          targetId : 目标dom元素id
+ *          columns : 列表项基础信息，注意列表默认首列启用checkbox，checkbox自动定义id作为val,所以无需重复为列表定义id属性或checkbox元素
+ *          dataUrl : 数据请求地址
+ *          serverResponseCode : 服务器后台操作成功的响应标志，注意后台返回的响应标志名约定为'serverResponseCode'
+ *          modal:{modalId:'xxid', modalCallback:function(data){..}}
+ *              modalId : 需要绑定至新增按钮和修改按钮的modalId
+ *              modalCallback : 用于绑定新增和修改按钮的响应逻辑
+ *                  result : 携带按钮参数和当前选中行的所有数据，若有数据的话。
+ *                      data : 当前选中行的所有数据
+ *                      btnType : {'create'|'update'}按钮类型
  * @param option {targetId:'#xxx','columns':[{'name':'与实体属性名一致','title','表格列标题'},...],'dataUrl':'数据请求地址'}
  * @constructor
  */
@@ -106,6 +114,10 @@ var DataTablePlus = function (option) {
         var targetId = option['targetId']; // 目标dom元素id
         var columns = option['columns']; // 列表基础信息
         var dataUrl = option['dataUrl']; // 数据请求地址
+        var serverResponseCode = option['serverResponseCode']; // 服务器响应状态码
+        var modalId = option['modalId'];
+        var modal = option['modal']; // 新增和更改启用的modal
+        var data; // 当前表格的全部数据
         var defaultColumns = [
             {
                 "data": "id",
@@ -119,7 +131,7 @@ var DataTablePlus = function (option) {
             var title = column['title'];
             defaultColumns.push({'data': name, 'name': name, 'title': title});
         } // 添加用户自定义列参数
-        var table = $(targetId).dataTable({
+        var table = $("#" + targetId).dataTable({
             "pagingType": "full_numbers",
             "bAutoWidth": false,
             "language": {
@@ -186,6 +198,8 @@ var DataTablePlus = function (option) {
                         }
                     }
                 });// 绑定表格内checkbox的选择事件
+
+                data = getTableData().data; // 缓存数据
             },
             "dom": '<"#datatable_wrapper.dataTables_wrapper form-inline dt-bootstrap no-footer datatable-plus"' +
             '<"row"<"col-sm-6" <"#datatable_btn_group">><"col-sm-6" <"#datatable_input_search">>>' +
@@ -225,13 +239,60 @@ var DataTablePlus = function (option) {
                             }
                             break; // 搜索
                         case "btn_datatable_create":
-                            console.log("create");
+                            $("#" + modal['modalId']).modal(); // 显示modal
+                            modal.modalCallback({'btnType': 'create', 'data': null});
                             break; // 新增
                         case "btn_datatable_update":
-                            console.log("update");
+                            var ids = [];
+                            var index;
+                            var i = 0;
+                            $("input[name='iCheckGroup']").each(function () {
+                                i++; // 定位选择框所在的行索引
+                                var val = $(this).val();
+                                if (val != "all" && $(this).is(':checked')) {
+                                    ids.push(val);
+                                    index = i - 2;
+                                }
+                            });
+
+                            if (ids.length == 1) {
+                                $("#" + modal['modalId']).modal(); // 显示modal
+                                modal.modalCallback({'btnType': 'update', 'data': data[index]});
+                            } else {
+                                Global.notify("操作提醒", "未选取有效数据或选中多条数据！", "warning")
+                            }
                             break; // 修改
                         case "btn_datatable_del":
-                            console.log("del");
+                            var ids = [];
+                            $("input[name='iCheckGroup']").each(function () {
+                                var val = $(this).val();
+                                if (val != "all" && $(this).is(':checked')) {
+                                    ids.push(val);
+                                }
+                            });
+                            if (ids.length > 0) {
+                                Global.warning("删除警告", "确定要删除选中的记录吗？", "danger", function () {
+                                    $.ajax({
+                                        type: "POST",
+                                        url: dataUrl,
+                                        dataType: "json",
+                                        data: {
+                                            '_method': 'delete',
+                                            'ids': ids
+                                        },
+                                        success: function (data) {
+                                            var code = data['serverResponseCode'];
+                                            if (code == 1001) {
+                                                reloadTable(); // 刷新表格
+                                                Global.notify("操作提示：", "删除成功！", "success");
+
+                                            } else {
+                                                Global.notify("操作提示：", "删除失败！", "error");
+                                            }
+                                        }
+                                    });
+                                });
+                            }
                             break; // 删除
                     }
                 });
@@ -245,6 +306,13 @@ var DataTablePlus = function (option) {
             table.ajax.reload(function (json) {
                 // 请求到的数据做相应的操作，如详情查看等
             }, true); // 刷新表格，分页信息重置
+        }
+
+        /**
+         * 获取表格数据
+         */
+        function getTableData() {
+            return table.ajax.json();
         }
 
         return table;
