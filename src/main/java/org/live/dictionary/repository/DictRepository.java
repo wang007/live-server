c32c3c74beaaab174c043745e56fc7fd650e4b1f
347255699 <13710414456@163.com>
2017-04-09 01:39:00 +0800

    暂时提交数据字典

--- src/main/java/org/live/dictionary/repository/DictRepository.java
+++ src/main/java/org/live/dictionary/repository/DictRepository.java
@@ -2,13 +2,11 @@ package org.live.dictionary.repository;
 
 
 import org.live.common.base.BaseRepository;
-import org.live.dictionary.entity.DictType;
 import org.live.dictionary.entity.Dictionary;
-import org.live.dictionary.entity.DictionaryVo;
+import org.live.dictionary.vo.DictionaryVo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
-import java.util.List;
 import java.util.Map;
 
 /**
